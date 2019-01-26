1e88bf00199ce61913ab3ba4d8e273a3b7de7772
Artem Shugayev <ashugayev@griddynamics.com>
2019-01-25 16:11:54 -0800

    Fixed issue with url resolve for store client

--- service/src/main/java/com/gd/azure/poc/service/clients/StoreClient.java
+++ service/src/main/java/com/gd/azure/poc/service/clients/StoreClient.java
@@ -1,7 +1,6 @@
 package com.gd.azure.poc.service.clients;
 
 import com.gd.azure.poc.domain.Store;
-import java.util.Collection;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +13,4 @@ public interface StoreClient {
     @GetMapping(path = "/{storeId}")
     Store getStore(
         @PathVariable("storeId") String storeIds);
-
-    @GetMapping(path = "/{storeIds}")
-    Collection<Store> getStores(
-        @PathVariable("storeIds") String[] storeIds);
 }
