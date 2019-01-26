838768e532a6b956259367bc3e384ba0780f0f35
Artem Shugayev <ashugayev@griddynamics.com>
2019-01-25 17:08:27 -0800

    Updated store front

--- service/src/main/java/com/gd/azure/poc/service/clients/StoreClient.java
+++ service/src/main/java/com/gd/azure/poc/service/clients/StoreClient.java
@@ -4,13 +4,11 @@ import com.gd.azure.poc.domain.Store;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient(name = "dao-spring-boot", url = "http://dao-spring-boot")
-@RequestMapping(value = "/stores")
 public interface StoreClient {
 
-    @GetMapping(path = "/{storeId}")
+    @GetMapping(path = "/stores/{storeId}")
     Store getStore(
         @PathVariable("storeId") String storeIds);
 }
--- store-spring-boot/src/main/java/com/gd/azure/poc/rest/StoreApplication.java
+++ store-spring-boot/src/main/java/com/gd/azure/poc/rest/StoreApplication.java
@@ -2,14 +2,14 @@ package com.gd.azure.poc.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = {"com.gd.azure.poc"})
 @ComponentScan(basePackages = {"com.gd.azure.poc"})
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class StoreApplication {
 
     public static void main(String[] args) {
