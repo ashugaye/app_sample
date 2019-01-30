9920e8e5e6b19e1298842012df7147bbf794337b
Artem Shugayev <artem.shugayev@walmartlabs.com>
2019-01-30 12:07:32 -0800

    Modified workload to run in 10K response size

--- workload-cache.sh
+++ workload-cache.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for i in {1..1000}; do combined=""; for i in {1..3}; do token=$((1 + RANDOM % 10000)); combined="${combined}${combined:+,}$token"; done; curl -s http://store-spring-boot/stores/$combined; done
+for i in {1..1000}; do combined=""; for i in {1..30}; do token=$((1 + RANDOM % 10000)); combined="${combined}${combined:+,}$token"; done; curl -s http://store-spring-boot/stores/$combined; done
